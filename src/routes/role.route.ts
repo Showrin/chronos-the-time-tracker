76904d7df8cf52816e66bcf8a18de4aca735268c
Showrin <showrinece15@gmail.com>
2024-01-18 01:48:20 +0600

    🔥 Remove unused code from role route file

--- src/routes/role.route.ts
+++ src/routes/role.route.ts
@@ -4,11 +4,6 @@ import { canAccess } from "../middlewares/auth.middleware";
 
 const roleRouter = express.Router();
 
-// roleRouter.post("/", RoleController.createRole);
 roleRouter.get("/", canAccess("*"), RoleController.getRoles);
-// roleRouter.get("/:roleId", RoleController.getRoleById);
-// roleRouter.put("/:roleId", RoleController.updateRoleById);
-// roleRouter.delete("/:roleId", RoleController.deleteRoleById);
-// roleRouter.put("/:roleId/reactivate", RoleController.reactivateRoleById);
 
 export { roleRouter };
